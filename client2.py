79b4cd421a2088a79bcb8d719527a2a3cc48745d
Ayanabha <87515557+ayan-cs@users.noreply.github.com>
2025-05-31 00:26:26 +0530

    client scripts

---
+++ client2.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST = '10.6.2.240' # Server IP
+PORT = 12345 # Port no. in Server
+
+def run_client():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
+        # Block-1
+        message = input("Enter the message:")
+        message = f"c2m1 {message}"
+        s.sendall(message.encode('utf-8'))
+        data = s.recv(1024)
+
+        # Block-2
+        message = input("Enter the message:")
+        message = f"c2m2 {message}"
+        s.sendall(message.encode('utf-8'))
+        data = s.recv(1024)
+
+        print(f"Server responded: {data.decode('utf-8')}")
+
+if __name__ == "__main__":
+    run_client()
